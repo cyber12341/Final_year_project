326d9c9333834235b07a2c2807a2c110ef4f079c
cyber12341 <redphantom2010@gmail.com>
2018-04-11 23:08:44 +0800

    profile coordinator layout

--- app/src/main/java/bemo/bemo/Profile.java
+++ app/src/main/java/bemo/bemo/Profile.java
@@ -1,13 +1,16 @@
 package bemo.bemo;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Profile extends AppCompatActivity {
-
+    CollapsingToolbarLayout collapsingToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        collapsingToolbar.setTitle("Suleiman Ali Shakir");
     }
 }
